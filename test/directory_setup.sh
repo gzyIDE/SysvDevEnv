3909d4d364ee045ee31a1a4df48d8e050c4f9735
Yosuke Ide <yosuke.ide@keio.jp>
2021-05-30 12:22:32 +0900

    Separated directory configuration from testvec.sh

---
+++ test/directory_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/tcsh
+
+# Files and Directories Settings
+set TOPDIR = ".."
+set RTLDIR = "${TOPDIR}/rtl"
+set TESTDIR = "${TOPDIR}/test"
+set TBDIR = "${TESTDIR}/tb"
+set TESTINCDIR = "${TESTDIR}/include"
+set GATEDIR = "${TOPDIR}/syn/result"
+set SV2VDIR = "${TOPDIR}/sv2v"
+set SV2VRTLDIR = "${SV2VDIR}/rtl"
+set SV2VTESTDIR = "${SV2VDIR}/test"
+set INCDIR = ( \
+	${TOPDIR}/include \
+	${TESTINCDIR} \
+)
+set DEFINE_LIST = ( \
+	SIMULATION \
+)
+set INCLUDE = ()
+set DEFINES = ()
+set RTL_FILE = ()
--- test/testvec.sh
+++ test/testvec.sh
@@ -1,25 +1,7 @@
 #!/bin/tcsh
 
 ##### File and Directory Settings #####
-set TOPDIR = ".."
-set RTLDIR = "${TOPDIR}/rtl"
-set TESTDIR = "${TOPDIR}/test"
-set TBDIR = "${TESTDIR}/tb"
-set TESTINCDIR = "${TESTDIR}/include"
-set GATEDIR = "${TOPDIR}/syn/result"
-set SV2VDIR = "${TOPDIR}/sv2v"
-set SV2VRTLDIR = "${SV2VDIR}/rtl"
-set SV2VTESTDIR = "${SV2VDIR}/test"
-set INCDIR = ( \
-	${TOPDIR}/include \
-	${TESTINCDIR} \
-)
-set DEFINE_LIST = ( \
-	SIMULATION \
-)
-set INCLUDE = ()
-set DEFINES = ()
-set RTL_FILE = ()
+source directory_setup.sh
 
 
 
