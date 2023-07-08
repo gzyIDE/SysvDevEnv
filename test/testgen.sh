6aa17f6d356f1b04a62aa2ec1c2bbac098a0f98e
Yosuke Ide <gizaneko@outlook.jp>
2023-07-08 23:34:05 +0900

    Added testbench template generator

---
+++ test/testgen.sh
@@ -0,0 +1,28 @@
+#!/bin/tcsh
+
+source directory_setup.sh
+source target.sh
+
+foreach dir ( $INCDIR )
+  set INCLUDE = ( \
+    -incdir=$dir \
+    $INCLUDE \
+  )
+end
+
+# Simulation Target Selection
+if ( $# =~ 0 ) then
+	set TOP_MODULE = $DEFAULT_DESIGN
+else
+	set TOP_MODULE = $1
+endif
+
+
+# Test bench generation
+set dmpfile = tb_dump.sv
+grep \`include ${RTLDIR}/${TOP_MODULE}.sv >! ${dmpfile}
+echo "" >> ${dmpfile}
+./gen/tbgen.pl \
+  -i ${RTLDIR}/${TOP_MODULE}.sv \
+  -t ${TOP_MODULE} \
+  ${INCLUDE} >> ${dmpfile}
